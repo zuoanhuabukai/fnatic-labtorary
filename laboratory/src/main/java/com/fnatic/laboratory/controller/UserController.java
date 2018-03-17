bf5180ed2c5dcf0c7cba971d048e8e362c8e3648
zuoanhuabukai <935718824@qq.com>
2018-03-17 11:34:08 +0800

    增加getUserById

--- laboratory/src/main/java/com/fnatic/laboratory/controller/UserController.java
+++ laboratory/src/main/java/com/fnatic/laboratory/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fnatic.laboratory.controller;
 
+import com.fnatic.laboratory.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,17 @@ public class UserController {
     @ResponseBody
     public String sayHello(){
         return "hello";
+    }
+
+    @RequestMapping("/getUserById")
+    @ResponseBody
+    public User getUserById(Long id){
+        User u = new User();
+        u.setId(1l);
+        u.setName("Tom");
+        u.setAge(20);
+
+        return u;
     }
 
 }
